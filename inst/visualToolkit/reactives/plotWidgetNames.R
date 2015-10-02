4edffaebf8384e381939e90d385b48eef12882ff
juliux <juliux@arranger.Home>
2015-10-02 15:22:38 +1000

    Density plot now displays when opening aesthetics widgets

--- inst/visualToolkit/reactives/plotWidgetNames.R
+++ inst/visualToolkit/reactives/plotWidgetNames.R
@@ -63,8 +63,6 @@ histogramWidgets <- reactive({
 ## density plot widgets
 densityWidgets <- reactive({
   wgts <- c('plotType', 'x')
-  if (input$showAesWgts)
-    wgts <- c(wgts, 'fill', 'color', 'densBlkLineCond')
   if (input$showXYRangeWgts)
     wgts <- c(wgts, 'xlim')
   if (input$showFacetWgts)
